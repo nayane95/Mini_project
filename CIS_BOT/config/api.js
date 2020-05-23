6990b9833695362ee774faac2072057c2584d859
Nayanajith Priyasad <nayanajithpriyasad@gmail.com>
2020-05-23 14:45:01 +0530

    first_commit

---
+++ CIS_BOT/config/api.js
@@ -0,0 +1,28 @@
+var apiai = require('apiai');
+
+
+//API Key
+var app = apiai('47b0b1fef83f44498191e9e7cd5cb95c');
+
+// Function which returns speech from dialogflow
+var getRes = function(query) {
+  var request = app.textRequest(query, {
+      sessionId: '<unique session id>'
+  });
+const responseFromAPI = new Promise(
+        function (resolve, reject) {
+request.on('error', function(error) {
+    reject(error);
+});
+request.on('response', function(response) {
+  resolve(response.result.fulfillment.speech);
+});
+});
+request.end();
+return responseFromAPI;
+};
+
+// test the command :
+//getRes('hello').then(function(res){console.log(res)});
+
+module.exports = {getRes}
